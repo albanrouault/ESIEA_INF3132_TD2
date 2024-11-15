47e6374a7995ea1fb2f5c1c9778d05f31f092c78
Banban <alban.rouault@hotmail.com>
2024-11-15 15:11:52 +0100

    UPDATE : Étape 2, création des classes (pas encore implémenté)

---
+++ src/main/java/com/esiea/pootd2/interfaces/TextInterface.java
@@ -0,0 +1,9 @@
+package com.esiea.pootd2.interfaces;
+
+public class TextInterface implements IUserInterface {
+
+    @Override
+    public void run() {
+        System.out.println("Hello World!");
+    }
+}
