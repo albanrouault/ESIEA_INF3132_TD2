b528a50fc1c9c532d721b613a67e66a89d053baa
Banban <alban.rouault@hotmail.com>
2024-11-17 17:06:17 +0100

    ADD : Étape 6

--- src/main/java/com/esiea/pootd2/controllers/ExplorerController.java
+++ src/main/java/com/esiea/pootd2/controllers/ExplorerController.java
@@ -2,10 +2,9 @@ package com.esiea.pootd2.controllers;
 
 import com.esiea.pootd2.commands.*;
 import com.esiea.pootd2.commands.parsers.UnixLikeCommandParser;
-import com.esiea.pootd2.models.FolderInode;
+import com.esiea.pootd2.models.*;
 
 public class ExplorerController implements IExplorerController {
-
     private FolderInode currentDirectory;
 
     public ExplorerController() {
@@ -33,23 +32,74 @@ public class ExplorerController implements IExplorerController {
         }
     }
 
+    /**
+     * Change the current directory to the given path.
+     * @param command The command to execute.
+     * @return The result of the command.
+     */
     private String doCommand(ChangeDirectoryCommand command) {
+        // Split the path into parts
+        String[] pathParts = command.getPath().split("/");
+        // Loop through the path parts to navigate to the desired folder
+        FolderInode currentFolder = currentDirectory;
+        for (String pathPart : pathParts) {
+            currentFolder = currentFolder.getFolderInode(pathPart);
+            if (currentFolder == null) {
+                return this.doCommand(new ErrorCommand("No such file or directory"));
+            }
+        }
+        currentDirectory = currentFolder;
         return "";
     }
 
+    /**
+     * Handle the error command.
+     * @param command The command to execute.
+     * @return The result of the command.
+     */
     private String doCommand(ErrorCommand command) {
-        return "";
+        return command.getMessage() + "\n";
     }
 
+    /**
+     * TODO : Voir si on peut rendre cette liste en RO avec une deep copy et si on doit pouvoir l'exec à distance, exemple ls /home/user
+     * Handle the list command.
+     * @param command The command to execute.
+     * @return The result of the command.
+     */
     private String doCommand(ListCommand command) {
-        return "";
+        // Loop through the children of the current folder and display their name and size
+        // Format : name(tabulation)size\n
+        StringBuilder result = new StringBuilder();
+        for (Inode child : currentDirectory.getChildrens()) {
+            result.append(child.getName()).append("\t").append(child.getSize()).append("\n");
+        }
+        return result.toString();
     }
 
+    /**
+     * TODO : Voir si on doit pouvoir créer un dossier à "distance" -> actuellement on stock le nom, dans ce cas il faudrait stocker le path
+     * Handle the make directory command.
+     * @param command The command to execute.
+     * @return The result of the command.
+     */
     private String doCommand(MakeDirectoryCommand command) {
+        // Create a new folder with the given name
+        FolderInode newFolder = new FolderInode(command.getDirectoryName());
+        currentDirectory.addInode(newFolder);
         return "";
     }
 
+    /**
+     * TODO : Voir si on doit pouvoir créer un fichier à "distance" -> actuellement on stock le nom, dans ce cas il faudrait stocker le path
+     * Handle the touch command.
+     * @param command The command to execute.
+     * @return The result of the command.
+     */
     private String doCommand(TouchCommand command) {
+        // Create a new file with the given name
+        FileInode newFile = new FileInode(command.getFileName());
+        currentDirectory.addInode(newFile);
         return "";
     }
 }
--- src/main/java/com/esiea/pootd2/models/FolderInode.java
+++ src/main/java/com/esiea/pootd2/models/FolderInode.java
@@ -3,25 +3,51 @@ package com.esiea.pootd2.models;
 import java.util.LinkedList;
 
 public class FolderInode extends Inode {
-    private final LinkedList<Inode> children;
+    private final LinkedList<Inode> childrens;
 
     public FolderInode(String name) {
         super(name);
 
-        this.children = new LinkedList<>();
+        this.childrens = new LinkedList<>();
     }
 
     public void addInode(Inode child) {
-        children.add(child);
+        childrens.add(child);
         child.parent = this;
+    }
+
+    /**
+     * Return the childrens of the folder.
+     * @return The childrens of the folder.
+     */
+    public LinkedList<Inode> getChildrens() {
+        return childrens;
     }
 
     @Override
     public int getSize() {
         int size = 0;
-        for (Inode child : children) {
+        for (Inode child : childrens) {
             size += child.getSize();
         }
         return size;
+    }
+
+    public FolderInode getFolderInode(String name) {
+        if (name.equals(".") || (name.equals("..") && this.getName().equals("/"))) {
+            return this;
+        }
+
+        if (name.equals("..")) {
+            return parent;
+        }
+
+        for (Inode child : childrens) {
+            if (child instanceof FolderInode folderInode && folderInode.getName().equals(name)) {
+                return folderInode;
+            }
+        }
+
+        return null;
     }
 }
