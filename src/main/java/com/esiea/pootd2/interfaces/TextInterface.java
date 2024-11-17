4e039df5d2fb3f6ba24a9979bebd9764456e6f01
Banban <alban.rouault@hotmail.com>
2024-11-17 17:16:22 +0100

    ADD : Étape 7

--- src/main/java/com/esiea/pootd2/interfaces/TextInterface.java
+++ src/main/java/com/esiea/pootd2/interfaces/TextInterface.java
@@ -1,8 +1,14 @@
 package com.esiea.pootd2.interfaces;
 
+import com.esiea.pootd2.controllers.ExplorerController;
 import java.util.Scanner;
 
 public class TextInterface implements IUserInterface {
+    private ExplorerController controller;
+
+    public TextInterface(ExplorerController controller) {
+        this.controller = controller;
+    }
 
     @Override
     public void run() {
@@ -14,23 +20,7 @@ public class TextInterface implements IUserInterface {
         while (true) {
             System.out.print("> ");
             input = scanner.nextLine();
-            String[] parts = input.split(" ");
-
-            // Pour le moment on met un switch mais si on plus tard dans l'exo on doit faire des classes pour les commandes, on utilisera sûrement du polymorphisme pour réduire le code et le rendre plus propre
-            switch (parts[0]) {
-                case "exit":
-                    scanner.close();
-                    return;
-                case "ls":
-                    break;
-                case "mkdir":
-                    break;
-                case "cd":
-                    break;
-                case "touch":
-                    break;
-                default:
-            }
+            System.out.println(controller.executeCommand(input));
         }
     }
 }
