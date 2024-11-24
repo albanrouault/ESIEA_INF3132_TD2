2ba71087c6717351d42f3271cfc08e4d9922e7fa
FarmVivi <farmvividev@gmail.com>
2024-11-24 16:08:29 +0100

    MakeDirectoryCommand doc

--- src/main/java/com/esiea/pootd2/commands/MakeDirectoryCommand.java
+++ src/main/java/com/esiea/pootd2/commands/MakeDirectoryCommand.java
@@ -7,18 +7,18 @@ public class MakeDirectoryCommand extends Command {
     private final String directoryPath;
 
     /**
-     * Constructs a MakeDirectoryCommand with the given directory name.
+     * Constructs a MakeDirectoryCommand with the given directory path.
      *
-     * @param directoryName The name of the directory to create.
+     * @param directoryPath The path of the directory to create.
      */
     public MakeDirectoryCommand(String directoryPath) {
         this.directoryPath = directoryPath;
     }
 
     /**
-     * Gets the name of the directory to create.
+     * Gets the path of the directory to create.
      *
-     * @return The name of the directory to create.
+     * @return The path of the directory to create.
      */
     public String getDirectoryPath() {
         return directoryPath;
